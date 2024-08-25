c40b6c883cb3e4f3bd4204f1f6a2375c8644f0ea
lseongmi <w2307@e-mirim.hs.kr>
2024-08-25 23:23:35 +0900

    상속과 rander 함수 사용

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import ImportComponent from './R003_ImportComponent';
 
 function App() {
   return (
---
+++ client/src/R003_ImportComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'; //component 임포ㄴ트
+
+class R003_ImportComponent extends Component {  //Component 상속받아 사용
+    render () {
+        return (
+            <h2>[ THIS IS IMPORTED COMPONENT ]</h2>
+        )
+    }
+}
+
+export default R003_ImportComponent;
