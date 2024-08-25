56910ab9654702da884af194b75a187085a92dbb
lseongmi <w2307@e-mirim.hs.kr>
2024-08-25 23:48:28 +0900

    rander() 생명주기 함수 사용

---
+++ client/src/R004_LifecycleEx.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class R004_LifecycleEx extends Component {
+    render() {
+        console.log('3. render Call');
+        return (
+            <h2>[ THIS IS RENDER FUNCION ]</h2>
+        )
+    }
+}
+
+export default R004_LifecycleEx;
